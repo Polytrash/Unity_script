8d3a53b69c6b75570013c290dd8a09a257858075
Polytrash <polytrash@visithp.com>
2015-11-20 03:34:07 +0900

    TransCount Unity Scriptable Object update Second Commit

---
+++ Unity_script/ScriptableObject/Resources/TransCount.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransCount : ScriptableObject {
+	
+	public int Value;
+
+	public Vector3 pos = new Vector3(0,0,0);
+	public Vector3 rot = new Vector3(0,0,0);
+	public Vector3 scal = new Vector3(1,1,1);
+
+	// Position Accessor
+
+	public Vector3 Pos
+	{
+		set{ pos = value;}
+		get{ return pos;}
+	}
+
+	public float PosX
+	{
+		set{ pos.x = value;}
+		get{ return pos.x;}
+	}
+
+	public float PosY
+	{
+		set{ pos.y = value;}
+		get{ return pos.y;}
+	}
+
+	public float PosZ
+	{
+		set{ pos.z = value;}
+		get{ return pos.z;}
+	}
+
+	// Rotation Accessor
+
+	public Vector3 Rot
+	{
+		set{ rot = value;}
+		get{return rot;}
+	}
+
+	public float RotX
+	{
+		set{ rot.x = value;}
+		get{ return rot.x;}
+	}
+
+	public float RotY
+	{
+		set{ rot.y = value;}
+		get{ return rot.y;}
+	}
+
+	public float RotZ
+	{
+		set{ rot.z = value;}
+		get{ return rot.z;}
+	}
+
+	// Scale Accessor
+
+	public float ScalX
+	{
+		set{ scal.x = value;}
+		get{ return scal.x;}
+	}
+
+	public float ScalY
+	{
+		set{ scal.y = value;}
+		get{ return scal.y;}
+	}
+
+	public float ScalZ
+	{
+		set{ scal.z = value;}
+		get{ return scal.z;}
+	}
+}
+
+
---
+++ Unity_script/ScriptableObject/Scripts/Cube1.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube1 : MonoBehaviour {
+	
+	public TransCount transCount;
+
+	
+	void Start () {
+		transCount = Resources.Load<TransCount>("TransCount");
+	}
+	
+	void OnGUI()
+	{
+		GUI.Label(new Rect(800, 5, 200, 80), this.transCount.Value.ToString());
+
+		GUI.Label(new Rect(800, 25, 200, 80), this.transCount.PosX.ToString());
+		GUI.Label(new Rect(900, 25, 200, 80), this.transCount.PosY.ToString());
+		GUI.Label(new Rect(1000, 25, 200, 80), this.transCount.PosZ.ToString());
+
+		GUI.Label(new Rect(800, 45, 200, 80), this.transCount.Rot.ToString());
+
+		GUI.Label(new Rect(800, 65, 200, 80), this.transCount.ScalX.ToString());
+		GUI.Label(new Rect(900, 65, 200, 80), this.transCount.ScalX.ToString());
+		GUI.Label(new Rect(1000, 65, 200, 80), this.transCount.ScalX.ToString());
+
+	}
+
+	void SetPosition(float vX, float vY, float vZ){
+		transform.position = new Vector3(vX, vY, vZ);
+		
+	}
+
+	void SetRotation(float vX, float vY, float vZ){
+		transform.localEulerAngles = new Vector3 (vX, vY, vZ);
+	}
+
+	void SetScale(float vX, float vY, float vZ){
+		transform.localScale = new Vector3 (vX, vY, vZ);
+	}
+
+	void OnMouseDown()
+	{
+
+		this.transCount.Value++;
+
+		transCount.PosX = GetComponent<Transform>().position.x ;
+		transCount.PosY = GetComponent<Transform>().position.y ;
+		transCount.PosZ = GetComponent<Transform>().position.z ;
+
+		transCount.Rot = GetComponent<Transform>().localEulerAngles;
+
+		transCount.ScalX = GetComponent<Transform>().localScale.x ;
+		transCount.ScalY = GetComponent<Transform>().localScale.y ;
+		transCount.ScalZ = GetComponent<Transform>().localScale.z ;
+
+		SetPosition (transCount.PosX, transCount.PosY, transCount.PosZ);
+		SetRotation (transCount.RotX, transCount.RotY, transCount.RotZ);
+		SetScale (transCount.ScalX, transCount.ScalY, transCount.ScalZ);
+		
+	}
+
+	void Update(){
+
+
+	}
+}
