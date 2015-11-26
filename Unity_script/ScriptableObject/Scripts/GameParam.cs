0d272b44bfd8a0ad34ddc2dd3f12e191409cad32
Polytrash <polytrash@visithp.com>
2015-11-27 03:22:23 +0900

    Scriptable object serializable sample First Commit

---
+++ Unity_script/ScriptableObject/Scripts/GameParam.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameParam : MonoBehaviour {
+	
+	public GameSetting gameSetting;
+
+	
+	void Start () {
+		gameSetting = Resources.Load<GameSetting>("GameSetting");
+	}
+	
+	void OnGUI()
+	{
+
+	}
+	
+
+
+
+}
---
+++ Unity_script/ScriptableObject/Scripts/GameSetting.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+
+
+public class EachLevel
+{
+	
+    public uint mLevelID;
+    public uint mSceneID;
+	
+};
+
+[System.Serializable]
+public class GameSetting : ScriptableObject
+{
+	
+	
+    public EachLevel[] lvl;
+	public void Init ( uint id1, uint id2 )
+		
+    {
+		
+         lvl = new EachLevel[2];
+         lvl[0].mLevelID = id1;
+		 lvl [0] = new EachLevel ();
+         lvl[0].mSceneID = id2;
+		 lvl [1] = new EachLevel ();
+         lvl[1].mLevelID = id2;
+         lvl[1].mSceneID = id1;
+     }
+	
+	
+}
+
+		
+
+
+	
+
