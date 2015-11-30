cf159308b2561174a48374ccdd806140cf22b665
Polytrash <polytrash@visithp.com>
2015-12-01 03:35:02 +0900

    Serializable parameter get set sample First Commit

---
+++ Unity_script/ScriptableObject/Scripts/bezierCurveDir.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+[System.Serializable]
+public class bezierCurveDir :MonoBehaviour {
+
+	public int a;
+	public dirPos p0;
+	public dirPos p1;
+	public dirPos p2;
+}
+
+
+[System.Serializable]
+public class dirPos {
+	public Vector3 pos;
+	public float dir;
+	public float Dir {get{return dir;}set{dir = value;}}
+};
+
---
+++ Unity_script/ScriptableObject/Scripts/bezierGet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class bezierGet : MonoBehaviour{
+
+
+	public Vector3 b;
+	public float d;
+	
+
+	void Awake(){
+
+		//GameObject GO = GameObject.Find ("bezierGet");
+		//GO.GetComponent<dirPos>().Dir = 10f;
+		GameObject GO = GameObject.Find ("bezierCurveDir");
+		GO.GetComponent<bezierCurveDir>().p0.Dir = 10000f;
+
+
+	}
+}
