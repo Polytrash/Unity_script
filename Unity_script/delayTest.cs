f8e994785f593cc509d4f47e6f0e603cb9dc443a
Polytrash <polytrash@visithp.com>
2016-02-03 03:08:15 +0900

    delayTest test First Commit

---
+++ Unity_script/delayTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class delayTest : MonoBehaviour {
+
+
+	private IEnumerator loop(int frame)
+	{
+		while (frame > 0) {
+
+			yield return null;
+			Debug.Log ("looop");
+			frame--;
+		}
+		doSomething ();
+	}
+
+	int a = 10;
+
+
+	private void doSomething(){
+	
+		Debug.Log ("Done!");
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+			StartCoroutine(loop(a));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	
+	}
+
+}
