74b38ae05d65dc6a5de9ed33cbe4bb363ddf6c0e
Polytrash <polytrash@visithp.com>
2017-02-17 03:45:59 +0900

    ShowNormal

---
+++ Unity_script/ShowNormal.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ShowNormal : MonoBehaviour {
+
+    public Mesh mesh;
+    public float normalLength = 0.1f;
+    public bool isRed;
+
+
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().sharedMesh;
+    }
+
+    void Update()
+    {
+        Color col = Color.green;
+
+        if (isRed) { col = Color.red; };
+
+        mesh = GetComponent<MeshFilter>().sharedMesh;
+        for (int i = 0; i < mesh.vertices.Length; i++)
+        {
+            Vector3 norm = transform.TransformDirection(mesh.normals[i]);
+            Vector3 vert = transform.TransformPoint(mesh.vertices[i]);
+            Debug.DrawRay(vert, norm * normalLength , col);
+        }
+    }
+}
